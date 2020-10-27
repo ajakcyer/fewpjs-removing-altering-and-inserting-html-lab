421fcf7ad60d2da67d7c2fe30a1efac8539ac144
ajakcyer <ajakcyer97@gmail.com>
2020-10-26 22:25:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const h1 = document.createElement('h1')
+
+document.body.appendChild(h1)
+h1.id = 'victory'
+
+const newHeader = document.querySelector('h1#victory')
+
+let name = "Ajak"
+
+newHeader.innerHTML = `${name} is the champion`
